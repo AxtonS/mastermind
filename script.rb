2e1f99a72ecf9f0ab8b314e482705914f3fe2ef3
Axton Smith <axtonsmith@gmail.com>
2022-06-22 18:48:54 -0400

    Build compare code method

--- script.rb
+++ script.rb
@@ -13,8 +13,19 @@ class CodeMaker
   include InitialVariables
   attr_reader :code
 
-  def random_code
+  def random
     4.times { @code.push(@colors.sample) }
+  end
+
+  def compare(guess)
+    full_key = 0
+    color_key = ((@code - guess).length - 4).abs
+    @code.each_with_index do |color, index|
+      if guess[index] == color
+        full_key += 1
+        color_key -= 1
+      end
+    end
   end
 end
 
@@ -35,16 +46,13 @@ class CodeBreaker
   end
 end
 
-secret = CodeMaker.new
-secret.random_code
-p secret.code
-puts 'Welcome to Mastermind!'
-puts 'The computer has generated a random code 4 items long, each item can be one of:'
-puts 'RED, ORANGE, YELLOW, GREEN, BLUE, or VIOLET'
-puts "\nYou have 12 guesses to figure out the code,"
-puts 'Between each guess the computer will tell you how many items are the right color and in the right place,'
-puts 'as well as how many are simply the right color but in the wrong place'
-puts "\nPlease enter your guess one color at a time, pressing enter between each color:"
-player = CodeBreaker.new
-player.input
-p player.code
+# secret = CodeMaker.new
+# secret.random
+# player = CodeBreaker.new
+# puts 'Welcome to Mastermind!'
+# puts 'The computer has generated a random code 4 items long, each item can be one of:'
+# puts 'RED, ORANGE, YELLOW, GREEN, BLUE, or VIOLET'
+# puts "\nYou have 12 guesses to figure out the code,"
+# puts 'Between each guess the computer will tell you how many items are the right color and in the right place,'
+# puts 'as well as how many are simply the right color but in the wrong place'
+# puts "\nPlease enter your guess one color at a time, pressing enter between each color:"
